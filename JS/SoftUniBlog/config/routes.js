593e558cb55d68ba8c351faf57742e8ac01b1408
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-11-15 07:45:59 +0200

    JS

--- JS/SoftUniBlog/config/routes.js
+++ JS/SoftUniBlog/config/routes.js
@@ -18,5 +18,7 @@ module.exports = (app) => {
 
     app.get('/article/details/:id', articleController.details);
 
+    app.get('/article/edit/:id', articleController.editGet);
+    app.get('/article/edit/:id', articleController.editPost);
 };
 
