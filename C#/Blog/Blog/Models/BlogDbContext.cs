a981e8883e153f8fa74fb34e9f2382883828bb4f
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-12-03 10:06:33 +0200

    C#

--- C#/Blog/Blog/Models/BlogDbContext.cs
+++ C#/Blog/Blog/Models/BlogDbContext.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Blog.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-
-
+    
     public class BlogDbContext : IdentityDbContext<ApplicationUser>
     {
         public BlogDbContext()
