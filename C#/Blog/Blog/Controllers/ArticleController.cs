23976d261c20768418a162ab24b0ce8aac0d81e2
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-12-06 19:39:05 +0200

    C#

--- C#/Blog/Blog/Controllers/ArticleController.cs
+++ C#/Blog/Blog/Controllers/ArticleController.cs
@@ -148,5 +148,68 @@ namespace Blog.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        // GET: Article/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get article from the database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .First();
+
+                // Check if article exists
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Create the view model
+                var model = new ArticleViewModel();
+                model.Id = article.Id;
+                model.Title = article.Title;
+                model.Content = article.Content;
+
+                // Pass the view model to view
+                return View(model);
+            }
+        }
+
+        // POST: Article/Edit
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            // Check if model state is valid
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    // Get Article from database
+                    var article = database.Articles
+                        .FirstOrDefault(a => a.Id == model.Id);
+
+                    // Set article properties
+                    article.Title = model.Title;
+                    article.Content = model.Content;
+
+                    // Save article state in database
+                    database.Entry(article).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    // Redirect to the index page
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // If model state is invalid, retyrn the same view
+
+            return View(model);
+        }
     }
 }
