7f7e25fae7105bc2b7f4e5430eb40d3898e67977
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-12-09 18:46:59 +0200

    C#

---
+++ C#/Blog/Blog/Controllers/Admin/ApplicationUserManager.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Controllers.Admin
+{
+    internal class ApplicationUserManager<T>
+    {
+        private UserStore<ApplicationUser> userStore;
+
+        public ApplicationUserManager(UserStore<ApplicationUser> userStore)
+        {
+            this.userStore = userStore;
+        }
+    }
+}
---
+++ C#/Blog/Blog/Controllers/Admin/UserController.cs
@@ -0,0 +1,184 @@
+﻿using Blog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Admin
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: User/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var users = database.Users
+                    .ToList();
+
+                var admins = GetAdminUserNames(users, database);
+                ViewBag.admins = admins;
+
+                return View(users);
+            }
+        }
+
+        // GET: User/Edit
+        public ActionResult Edit(string id)
+        {
+            // Validate Id 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get user from database
+                var user = database.Users
+                    .Where(u => u.Id == id)
+                    .First();
+
+                // Check if user exists
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Create a view model
+                var viewModel = new EditUserViewModel();
+                viewModel.User = user;
+                viewModel.Roles = GetUserRoles(user, database);
+
+                // Pass the model to the view
+                return View(viewModel);
+            }
+
+
+        }
+
+        // POST: User/Edit
+
+        [HttpPost]
+        public ActionResult Edit(string id, EditUserViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    // Get user from database
+                    var user = database.Users.FirstOrDefault(u => u.Id == id);
+
+                    // Check if user axist
+                    if (user == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    // If password field is not empty, change password
+                    if (!string.IsNullOrEmpty(viewModel.Password))
+                    {
+                        var hasher = new PasswordHasher();
+                        var passwordHash = hasher.HashPassword(viewModel.Password);
+                        user.PasswordHash = passwordHash;
+                    }
+
+                    // Set user properties
+                    user.Email = viewModel.User.Email;
+                    user.FullName = viewModel.User.FullName;
+                    user.UserName = viewModel.User.Email;
+                    this.SetUserRoles(viewModel, user, database);
+
+                    // Save changes
+                    database.Entry(user).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("List");
+                }
+            }
+            return View(viewModel);
+        }
+
+        private void SetUserRoles(EditUserViewModel model, ApplicationUser user, BlogDbContext db)
+        {
+            var userManager = Request
+               .GetOwinContext()
+               .GetUserManager<ApplicationUserManager>();
+
+            foreach (var role in model.Roles)
+            {
+                if (role.IsSelected)
+                {
+                    userManager.AddToRole(user.Id, role.Name);
+                }
+                else if (!role.IsSelected)
+                {
+                    userManager.RemoveFromRole(user.Id, role.Name);
+                }
+            }
+        }
+
+        private List<Role> GetUserRoles(ApplicationUser user, BlogDbContext db)
+        {
+            // Create user manager
+            var userManager = Request
+                .GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            // Get all applicatio roles
+            var roles = db.Roles
+                .Select(r => r.Name)
+                .OrderBy(r => r)
+                .ToList();
+
+            // For each application role, check if the user has it
+            var userRoles = new List<Role>();
+
+            foreach (var roleName in roles)
+            {
+                var role = new Role { Name = roleName };
+
+                if (userManager.IsInRole(user.Id, roleName))
+                {
+                    role.IsSelected = true;
+                }
+
+                userRoles.Add(role);
+            }
+
+            // Return a list with all roles
+            return userRoles;
+            
+        }
+
+        private HashSet<string> GetAdminUserNames(List<ApplicationUser> users, BlogDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+
+            var admins = new HashSet<string>();
+
+            foreach (var user in users)
+            {
+                if (userManager.IsInRole(user.Id, "Admin"))
+                {
+                    admins.Add(user.UserName);
+                }
+            }
+
+            return admins;
+        }
+    }
+}
