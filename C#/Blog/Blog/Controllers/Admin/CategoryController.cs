fe57916c4c3290dc7d0f1fadf9998be3ce875bfa
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2016-12-10 13:18:05 +0200

    C#

---
+++ C#/Blog/Blog/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers.Admin
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+        // GET: Category/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var categories = database.Categories
+                    .ToList();
+                return View(categories);
+            }
+        }
+    }
+}
